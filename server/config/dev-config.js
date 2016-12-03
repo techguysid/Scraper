f84e9ab57a7b6b9a6af1374efd21d6b3f66299f9
Siddharth Udeniya <techguysid.23@gmail.com>
2016-12-03 11:08:10 +0530

    Preset configurations for scraper

---
+++ server/config/dev-config.js
@@ -0,0 +1,6 @@
+/*Dev Enviornment Configurations*/
+
+const targetURL = 'https://medium.com';  //Target URL which is to be scraped
+const maxConcurrency = 5;  //Max Request to Target URL at any given moment of time
+const targetPages = 15;  //No of pages to be scraped
+const outputFile = 'MediumLinks.csv'; //Write output stream to output file
